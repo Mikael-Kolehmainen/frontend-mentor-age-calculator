963e54462bd11b2e67315dfe91a49d62482d997d
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2024-04-04 17:48:21 +0300

    Base html for app

--- src/public_site/controller/HomeController.php
+++ src/public_site/controller/HomeController.php
@@ -7,7 +7,42 @@ class HomeController
   public function showHomePage(): void
   {
     echo "
-
+      <section>
+        <article>
+          <form>
+            <div class='age-input'>
+              <label>DAY</label>
+              <input type='number' placeholder='DD'></type>
+            </div>
+            <div class='age-input'>
+              <label>MONTH</label>
+              <input type='number' placeholder='MM'></type>
+            </div>
+            <div class='age-input'>
+              <label>YEAR</label>
+              <input type='number' placeholder='YYYY'></type>
+            </div>
+          </form>
+          <div class='style-line'></div>
+          <a class='submit-btn'>
+            <i class='arrow' />
+          </a>
+          <div class='age-result'>
+            <div class='result'>
+              <p id='year' class='highlight'>--</p>
+              <p>years</p>
+            </div>
+            <div class='result'>
+              <p id='month' class='highlight'>--</p>
+              <p>months</p>
+            </div>
+            <div class='result'>
+              <p id='day' class='highlight'>--</p>
+              <p>days</p>
+            </div>
+          </div>
+        </article>
+      </section>
     ";
   }
 }
